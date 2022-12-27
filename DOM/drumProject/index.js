f413367e797167f90ce25ac43f17ed959bb89db6
Grégory Marini <greg.marini@hotmail.fr>
2022-12-27 11:55:43 +0100

    add sound on click event and on keydown event

--- DOM/drumProject/index.js
+++ DOM/drumProject/index.js
@@ -1,11 +1,43 @@
 const drumButtons = document.querySelectorAll(".drum");
 
 function handleClick(key) {
-    alert(`${key.toUpperCase()} got clicked!`);
+    let sound;
+    switch (key) {
+        case "d":
+            sound = "/sounds/crash.mp3";
+            break;
+        case "f":
+            sound = "/sounds/kick-bass.mp3";
+            break;
+        case "g":
+            sound = "/sounds/snare.mp3";
+            break;
+        case "h":
+            sound = "/sounds/tom-1.mp3";
+            break;
+        case "j":
+            sound = "/sounds/tom-2.mp3";
+            break;
+        case "k":
+            sound = "/sounds/tom-3.mp3";
+            break;
+        case "l":
+            sound = "/sounds/tom-4.mp3";
+            break;
+        default:
+            sound = undefined;
+            break;
+    }
+    audio = sound && new Audio(sound);
+    audio.play();
 }
 
 drumButtons.forEach((button) => {
     button.addEventListener("click", (e) => {
         handleClick(e.target.textContent);
     });
+    document.addEventListener("keydown", (e) => {
+        e.key === button.textContent && handleClick(e.key);
+    });
 });
+l;
