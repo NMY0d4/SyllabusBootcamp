6d5549ad56b3dfa6b707750705db11be39da1fc6
Grégory Marini <greg.marini@hotmail.fr>
2022-12-28 15:47:57 +0100

    review on route

--- nodeJs/my-express-server/server.js
+++ nodeJs/my-express-server/server.js
@@ -2,6 +2,29 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res) => {
+    console.log(req);
+    res.send("<h1>Hello, world!</h1>");
+});
+
+app.get("/contact", (req, res) => {
+    console.log(req);
+    res.send("<h1>contact me at: greg.marini@hotmail.fr</h1>");
+});
+
+app.get("/about", (req, res) => {
+    console.log(req);
+    res.send(
+        "<h1>I'm Grégory Marini</h1><p>A web developper... fullstack...</p>"
+    );
+});
+app.get("/hobbies", (req, res) => {
+    console.log(req);
+    res.send(
+        "<h1>My Hobbies: </h1><ul><li>Sciences</li><li>code</li><li>Video games(reflexions)</li></ul>"
+    );
+});
+
 app.listen(3000, () => {
     console.log(`Server started on port 3000`);
 });
