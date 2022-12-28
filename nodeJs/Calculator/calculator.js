1b6dfcac430b79d3e5115ddf19e77ed727ff33e1
Grégory Marini <greg.marini@hotmail.fr>
2022-12-28 21:35:02 +0100

    training on formulaire and req/res(client->server)

--- nodeJs/Calculator/calculator.js
+++ nodeJs/Calculator/calculator.js
@@ -1,14 +1,25 @@
 const dotenv = require("dotenv");
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 dotenv.config();
 
-console.log(process.env.PORT);
 const PORT = process.env.PORT;
 
 app.get("/", (req, res) => {
-    res.send("<h1>Calculator</h1>");
+    console.log(`${__dirname}/index.html`);
+    res.sendFile(`${__dirname}/index.html`);
+});
+
+app.post("/", (req, res) => {
+    const num1 = +req.body.num1;
+    const num2 = +req.body.num2;
+
+    const result = num1 + num2;
+    res.send(`The result of the calculation is ${result}`);
 });
 
 app.listen(PORT, () => {
