4215fa7bb48dd79dc7887bd91e3cd66e2dc85d56
Grégory Marini <greg.marini@hotmail.fr>
2023-01-01 19:43:35 +0100

    add mailchimp api

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
 const dotenv = require("dotenv");
+const https = require("https");
 
 const app = express();
 dotenv.config();
@@ -17,7 +18,36 @@ app.get("/", (req, res) => {
 
 app.post("/", (req, res) => {
     const { firstName, lastName, email } = req.body;
-    console.log(firstName, lastName, email);
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName,
+                },
+            },
+        ],
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    const url = `https://us13.api.mailchimp.com/3.0/lists/${process.env.MAILCHIMP_UNIQID}`;
+
+    const options = {
+        method: "POST",
+        auth: "greg1:4aea1a77435589445a8302725bac1e20-us13",
+    };
+
+    const request = https.request(url, options, (res) => {
+        res.on("data", (data) => {
+            console.log(JSON.parse(data));
+        });
+    });
+
+    request.write(jsonData);
+    request.end();
 });
 
 app.listen(PORT, console.log(`server is running on port ${PORT}`));
