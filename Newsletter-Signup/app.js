0cda69be6eab8110d5f0ca040afcb063eb7663c8
Grégory Marini <greg.marini@hotmail.fr>
2023-01-01 18:03:58 +0100

    formatting form and post method

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -1,10 +1,23 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
 const dotenv = require("dotenv");
-const https = require("https");
 
 const app = express();
 dotenv.config();
 
 const PORT = process.env.PORT;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.sendFile(`${__dirname}/signup.html`);
+});
+
+app.post("/", (req, res) => {
+    const { firstName, lastName, email } = req.body;
+    console.log(firstName, lastName, email);
+});
 
 app.listen(PORT, console.log(`server is running on port ${PORT}`));
