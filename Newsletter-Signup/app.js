baefe28a829930602b1aefe7c3e90a4b23266329
Grégory <greg.marini@hotmail.fr>
2023-01-02 11:43:52 +0100

    solve security issue

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -13,41 +13,43 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.sendFile(`${__dirname}/signup.html`);
+  res.sendFile(`${__dirname}/signup.html`);
 });
 
 app.post("/", (req, res) => {
-    const { firstName, lastName, email } = req.body;
-    const data = {
-        members: [
-            {
-                email_address: email,
-                status: "subscribed",
-                merge_fields: {
-                    FNAME: firstName,
-                    LNAME: lastName,
-                },
-            },
-        ],
-    };
+  const { firstName, lastName, email } = req.body;
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
 
-    const jsonData = JSON.stringify(data);
+  const jsonData = JSON.stringify(data);
 
-    const url = `https://us13.api.mailchimp.com/3.0/lists/${process.env.MAILCHIMP_UNIQID}`;
+  const url = `https://us13.api.mailchimp.com/3.0/lists/${process.env.MAILCHIMP_UNIQID}`;
 
-    const options = {
-        method: "POST",
-        auth: "greg1:4aea1a77435589445a8302725bac1e20-us13",
-    };
+  const auth = `greg1:${process.env.API_KEY_MAILCHIMP}`;
 
-    const request = https.request(url, options, (res) => {
-        res.on("data", (data) => {
-            console.log(JSON.parse(data));
-        });
+  const options = {
+    method: "POST",
+    auth,
+  };
+
+  const request = https.request(url, options, (res) => {
+    res.on("data", (data) => {
+      console.log(JSON.parse(data));
     });
+  });
 
-    request.write(jsonData);
-    request.end();
+  request.write(jsonData);
+  request.end();
 });
 
 app.listen(PORT, console.log(`server is running on port ${PORT}`));
