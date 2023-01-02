cd6e3907a56a0f2fc6aff2742cb7a92ac889404d
Grégory Marini <greg.marini@hotmail.fr>
2023-01-02 15:32:24 +0100

    added success and failure page

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -14,47 +14,51 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.sendFile(`${__dirname}/signup.html`);
+    res.sendFile(`${__dirname}/signup.html`);
 });
 
 app.post("/", (req, res) => {
-  const { firstName, lastName, email } = req.body;
-  const data = {
-    members: [
-      {
-        email_address: email,
-        status: "subscribed",
-        merge_fields: {
-          FNAME: firstName,
-          LNAME: lastName,
-        },
-      },
-    ],
-  };
+    const { firstName, lastName, email } = req.body;
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName,
+                },
+            },
+        ],
+    };
 
-  const jsonData = JSON.stringify(data);
+    const jsonData = JSON.stringify(data);
 
-  const url = `https://us13.api.mailchimp.com/3.0/lists/${process.env.MAILCHIMP_UNIQID}`;
+    const url = `https://us13.api.mailchimp.com/3.0/lists/${process.env.MAILCHIMP_UNIQID}`;
 
-  const auth = `greg1:${process.env.API_KEY_MAILCHIMP}`;
+    const auth = `greg1:i${process.env.API_KEY_MAILCHIMP}`;
 
-  const options = {
-    method: "POST",
-    auth,
-  };
+    const options = {
+        method: "POST",
+        auth,
+    };
 
-  const request = https.request(url, options, (response) => {
-    response.statusCode === 200
-      ? res.send("Successfully subscribed!")
-      : res.send("There was an error with singing up, please try again!");
+    const request = https.request(url, options, (response) => {
+        response.statusCode === 200
+            ? res.sendFile(`${__dirname}/success.html`)
+            : res.sendFile(`${__dirname}/failure.html`);
 
-    response.on("data", (data) => {
-      console.log(JSON.parse(data));
+        response.on("data", (data) => {
+            console.log(JSON.parse(data));
+        });
     });
-  });
 
-  request.write(jsonData);
-  request.end();
+    request.write(jsonData);
+    request.end();
+});
+
+app.post("/failure", (req, res) => {
+    res.redirect("/");
 });
 
 app.listen(PORT, console.log(`server is running on port ${PORT}`));
