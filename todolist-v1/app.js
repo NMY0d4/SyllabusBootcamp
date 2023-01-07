2a4f0fe81275a41ae5d1e4b02c417cf1497ed61a
Grégory Marini <greg.marini@hotmail.fr>
2023-01-07 21:56:19 +0100

    instal ejs and launch first view

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -1,12 +1,27 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+const path = require("path");
+
+dotenv.config();
 
 const app = express();
 
-const PORT = 3000;
+app.set("view engine", "ejs");
+
+const PORT = process.env.PORT;
 
 app.get("/", (req, res) => {
-    res.send("Hello");
+    const today = new Date();
+    const currentDay = today.getDay();
+    let day = "";
+
+    if (currentDay === 6 || currentDay === 0) {
+        day = "week-end day";
+    } else {
+        day = "weekday";
+    }
+    res.render("list", { day });
 });
 
 app.listen(PORT, () => {
