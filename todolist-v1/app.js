ccae873cfd7888eae4f3055c69222b0ed1c5ba77
Grégory Marini <greg.marini@hotmail.fr>
2023-01-08 14:32:15 +0100

    add public folder for static path and style

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
-const path = require("path");
+// const path = require("path");
 
 dotenv.config();
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+let listItems = [];
 
 app.set("view engine", "ejs");
 
@@ -13,38 +18,22 @@ const PORT = process.env.PORT;
 
 app.get("/", (req, res) => {
     const today = new Date();
-    const currentDay = today.getDay();
-    let day = "";
 
-    switch (currentDay) {
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Saturday";
-            break;
-        case 0:
-            day = "Sunday";
-            break;
+    const options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+    };
 
-        default:
-            day = "Error";
-            break;
-    }
+    let day = today.toLocaleDateString("en-US", options);
 
-    res.render("list", { day });
+    res.render("list", { day, listItems });
+});
+
+app.post("/", (req, res) => {
+    const newItem = req.body.newItem;
+    listItems.push(newItem);
+    res.redirect("/");
 });
 
 app.listen(PORT, () => {
