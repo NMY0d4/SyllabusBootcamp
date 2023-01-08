bd26cb916389edbc6a97f478bcf390e080ead04b
Grégory Marini <greg.marini@hotmail.fr>
2023-01-08 11:02:04 +0100

    learn ejs

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -16,11 +16,34 @@ app.get("/", (req, res) => {
     const currentDay = today.getDay();
     let day = "";
 
-    if (currentDay === 6 || currentDay === 0) {
-        day = "week-end day";
-    } else {
-        day = "weekday";
+    switch (currentDay) {
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day = "Saturday";
+            break;
+        case 0:
+            day = "Sunday";
+            break;
+
+        default:
+            day = "Error";
+            break;
     }
+
     res.render("list", { day });
 });
 
