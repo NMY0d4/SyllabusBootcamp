f8d20027b833be31d6dd90bfa7e85aeddfc3e76f
Grégory Marini <greg.marini@hotmail.fr>
2023-01-11 10:54:20 +0100

    achieve until challenge 18(all gold star) + installing lodash

--- ejs-challenge/app.js
+++ ejs-challenge/app.js
@@ -8,6 +8,7 @@ const ejs = require("ejs");
 dotenv.config();
 
 const PORT = process.env.PORT;
+const posts = [];
 
 const homeStartingContent =
     "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -24,7 +25,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.render("home", { startingContent: homeStartingContent });
+    res.render("home", { startingContent: homeStartingContent, posts });
 });
 
 app.get("/about", (req, res) => {
@@ -38,11 +39,23 @@ app.get("/contact", (req, res) => {
 app.get("/compose", (req, res) => {
     res.render("compose");
 });
-app.post("/compose", (req, res) => {
-    const post = ({ postTitle, postText } = req.body);
 
-    console.log(post);
-    res.redirect("/compose");
+app.post("/compose", (req, res) => {
+    const post = ({ title, content } = req.body);
+
+    posts.push(post);
+
+    res.redirect("/");
+});
+
+app.get("/posts/:title", (req, res) => {
+    posts.forEach((post) =>
+        req.params.title === post.title
+            ? console.log("match found")
+            : console.log("not a match")
+    );
+
+    // res.render("post");
 });
 
 app.listen(PORT, function () {
