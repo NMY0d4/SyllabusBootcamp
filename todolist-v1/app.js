ea621707e85aee39faf99b078accfc8b65a2f030
Grégory <greg.marini@hotmail.fr>
2023-01-17 11:08:42 +0100

    work on error system, testing different things

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -12,34 +12,35 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 mongoose.connection.once("open", () => {
-    console.log("MongoDB Connection ready!");
+  console.log("MongoDB Connection ready!");
 });
 
 mongoose.connect(process.env.MONGODB_URL);
 
 /////////////////////////////////////////////////////////////////
 const itemsSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-    },
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
 });
 
 const Item = mongoose.model("Item", itemsSchema);
 
 const item1 = new Item({
-    name: "maîtriser mongoDB",
+  name: "maîtriser mongoDB",
 });
 
 const item2 = new Item({
-    name: "maîtriser nodeJS",
+  name: "maîtriser nodeJS",
 });
 
 const defaultItems = [item1, item2];
 
 const listSchema = {
-    name: String,
-    items: [itemsSchema],
+  name: String,
+  items: [itemsSchema],
 };
 
 const List = mongoose.model("List", listSchema);
@@ -53,93 +54,100 @@ app.set("view engine", "ejs");
 const PORT = process.env.PORT;
 
 app.get("/", (req, res) => {
-    Item.find({}, (err, foundItems) => {
-        if (foundItems.length === 0) {
-            Item.insertMany(defaultItems, function (err) {
-                if (err) {
-                    console.error(err);
-                } else {
-                    console.log("Successfully saved default items to DB.");
-                }
-            });
-            res.redirect("/");
-        } else {
-            res.render("list", {
-                listTitle: day(),
-                newListItems: foundItems,
-                errorItem,
-            });
-        }
-    });
+  Item.find({}, (err, foundItems) => {
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems)
+        .then(() => {
+          console.log("Successfully saved default items to DB.");
+        })
+        .catch((err) => {
+          errorItem = err.message;
+          res.redirect("/");
+        });
+      res.redirect("/");
+    } else {
+      res.render("list", {
+        listTitle: day(),
+        newListItems: foundItems,
+        errorItem,
+      });
+    }
+  });
 });
 
 app.post("/", (req, res) => {
-    errorItem = "";
-    const newItem = req.body.newItem;
-    const itemName = req.body.newItem;
-    const listName = req.body.list;
-    const item = new Item({ name: itemName });
+  const newItem = req.body.newItem;
+  const itemName = req.body.newItem;
+  const listName = req.body.list;
+  const item = new Item({ name: itemName });
 
-    if (!newItem) {
-        errorItem = "you must enter an item...";
-        return res.redirect("/");
-    }
+  if (!newItem) {
+    errorItem = "you must enter an item...";
+    return res.redirect("/");
+  }
 
-    if (listName === day()) {
-        item.save();
-        res.redirect("/");
-    } else {
-        List.findOne({ name: listName }, function (err, foundList) {
-            if (err) {
-                console.error(err);
-            } else {
-                foundList.items.push(item);
-                foundList.save();
-                res.redirect(`/${listName}`);
-            }
-        });
-    }
+  if (listName === day()) {
+    item
+      .save()
+      .then(() => {
+        console.log("Successfully saved default items to DB.");
+      })
+      .catch((err) => {
+        return (errorItem = `Err${err.code}: Item already existing...`);
+      });
+    res.redirect("/");
+  } else {
+    List.findOne({ name: listName }, function (err, foundList) {
+      if (err) {
+        console.error(err);
+      } else {
+        foundList.items.push(item);
+        foundList.save();
+        res.redirect(`/${listName}`);
+      }
+    });
+  }
 });
 
 app.post("/delete", (req, res) => {
-    const checkedItemId = req.body.checkbox;
-    Item.findByIdAndRemove(checkedItemId, function (err) {
-        if (err) {
-            console.error(err);
-        } else {
-            console.log("item successfully deleted...");
-        }
-        res.redirect("/");
-    });
+  const checkedItemId = req.body.checkbox;
+  Item.findByIdAndRemove(checkedItemId, function (err) {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log("item successfully deleted...");
+    }
+    res.redirect("/");
+  });
 });
 
 app.get("/:customListName", (req, res) => {
-    if (req.params.customListName !== "favicon.ico") {
-        const customListName = req.params.customListName;
+  if (req.params.customListName !== "favicon.ico") {
+    const customListName = req.params.customListName;
 
-        List.findOne({ name: customListName }, function (err, foundList) {
-            if (!err) {
-                if (!foundList) {
-                    // Create a new list
-                    const list = new List({
-                        name: customListName,
-                        items: defaultItems,
-                    });
-                    list.save();
-                    res.redirect(`/${customListName}`);
-                } else {
-                    //Show an existing list
-                    res.render("list", {
-                        listTitle: foundList.name,
-                        newListItems: foundList.items,
-                        errorItem,
-                    });
-                }
-            }
-        });
-    }
+    List.findOne({ name: customListName }, function (err, foundList) {
+      if (!err) {
+        if (!foundList) {
+          // Create a new list
+          const list = new List({
+            name: customListName,
+            items: defaultItems,
+          });
+          list.save();
+          res.redirect(`/${customListName}`);
+        } else {
+          //Show an existing list
+          res.render("list", {
+            listTitle: foundList.name,
+            newListItems: foundList.items,
+            errorItem,
+          });
+        }
+      }
+    });
+  }
 });
 
 app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}...`);
+  console.log(`Server started on port ${PORT}...`);
 });
