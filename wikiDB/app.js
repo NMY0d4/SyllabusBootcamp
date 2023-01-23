a533de008d9cc63cdc8aa22b87fd8386eba48c7b
Grégory Marini <greg.marini@hotmail.fr>
2023-01-23 22:31:27 +0100

    finish and testing addOneArticle with postman

--- wikiDB/app.js
+++ wikiDB/app.js
@@ -7,27 +7,6 @@ require("./services/mongo").mongoConnect();
 const path = require("path");
 const articlesRouter = require("./routes/articles/article.router");
 
-//////////////////////////////////////////////////////
-////      Créer des articles
-// const Article = require("./models/articles.model"):
-// Article.create({
-//   title: "API",
-//   content:
-//     "API stands for Application Programming Interface. It is a set of subrouting definitions, communication protocols, and tools for building software. In general terms, it is a set of clearly defined methods of communication among various components. A good API makes it easier to develop a computer program by providing all the building blocks, which are then put together by the programmer.",
-// });
-// Article.create({
-//   title: "Bootstrap",
-//   content:
-//     "This is a framework developed by Twitter that contains pre-made front-end templates for web design.",
-// });
-// Article.create({
-//   title: "DOM",
-//   content:
-//     "The Document Object Model is like an API for interacting with our HTML.",
-// });
-
-////////////////////////////////////////////////////
-
 const app = express();
 
 app.use(express.json());
@@ -44,9 +23,9 @@ const PORT = process.env.PORT;
 app.use("/articles", articlesRouter);
 
 app.get("/", (req, res) => {
-  res.render("home", {});
+    res.render("home", {});
 });
 
 app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}...`);
+    console.log(`Server started on port ${PORT}...`);
 });
--- wikiDB/routes/articles/article.controller.js
+++ wikiDB/routes/articles/article.controller.js
@@ -1,21 +1,23 @@
 const Article = require("../../models/articles.model");
 
 const getAllArticles = async (req, res) => {
-  try {
-    return await Article.find({}, (err, foundArticles) => {
-      res.send(foundArticles);
-    });
-  } catch (err) {
-    console.error(err.message);
-  }
+    try {
+        return await Article.find({}, (err, foundArticles) => {
+            res.send(foundArticles);
+        });
+    } catch (err) {
+        console.error(err.message);
+    }
 };
-const addOneArticle = async (req, res) => {
-  console.log(req.body);
-  //   try {
-  //     await Article.create(req.body, (err,newArticle) = {
-  //         res.status()
-  //     })
-  //   } catch (err) {}
+const addOneArticle = async (req, res, next) => {
+    const newArticle = req.body;
+    try {
+        await Article.create(newArticle);
+        res.status(201).send(newArticle);
+    } catch (err) {
+        console.error(err.message);
+    }
+    next();
 };
 
 module.exports = { getAllArticles, addOneArticle };
