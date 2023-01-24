686ff137839583a0335498b6a3ef137c0440e7aa
Grégory <greg.marini@hotmail.fr>
2023-01-24 09:42:07 +0100

    refactoring(article.controller)

--- wikiDB/routes/articles/article.controller.js
+++ wikiDB/routes/articles/article.controller.js
@@ -1,23 +1,39 @@
 const Article = require("../../models/articles.model");
 
 const getAllArticles = async (req, res) => {
-    try {
-        return await Article.find({}, (err, foundArticles) => {
-            res.send(foundArticles);
-        });
-    } catch (err) {
-        console.error(err.message);
-    }
+  try {
+    const articles = await Article.find();
+
+    res.status(200).json({
+      status: "success",
+      results: articles.length,
+      data: {
+        articles,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
 };
 const addOneArticle = async (req, res, next) => {
-    const newArticle = req.body;
-    try {
-        await Article.create(newArticle);
-        res.status(201).send(newArticle);
-    } catch (err) {
-        console.error(err.message);
-    }
-    next();
+  try {
+    const newArticle = await Article.create(req.body);
+    res.status(201).json({
+      status: "success",
+      data: {
+        newArticle,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: "invalid data sent!",
+    });
+  }
+  next();
 };
 
 module.exports = { getAllArticles, addOneArticle };
