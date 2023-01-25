bbfaef3e47364ca14a38784541cb6aa3865b39a1
Grégory Marini <greg.marini@hotmail.fr>
2023-01-25 22:27:20 +0100

    all start parameters ok

--- secret-app/app.js
+++ secret-app/app.js
@@ -1,1 +1,29 @@
-//jshint esversion:6
+require("dotenv").config();
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+app.get("/register", (req, res) => {
+    res.render("register");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}...`);
+});
